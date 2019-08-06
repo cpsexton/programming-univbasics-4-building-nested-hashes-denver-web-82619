6dc937269dc8979d73bd9ed58bc248f81567b508
Christopher Sexton <cpsexton@gmail.com>
2019-08-06 19:32:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,12 @@ def monopoly_with_third_tier
  {
   :railroads => {
     :pieces => 4,
-    :rent_in_dollars => {:1,:2,:3,:4,}
+    :rent_in_dollars => {
+      :onepice => 1,
+      :twop => 2,
+      :thee => 3,
+      :four => 4
+    }
   }
 }
 end
