2a47986ba099b36932de0b16aa8a583699531eda
Christopher Sexton <cpsexton@gmail.com>
2019-08-06 19:38:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,9 +17,9 @@ def monopoly_with_third_tier
     :pieces => 4,
     :rent_in_dollars => {
       :one_piece_owned => 25,
-      :twop => 2,
-      :thee => 3,
-      :four => 4,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 75,
+      :four_pieces_owned => 100,
     },
     :names => {
       :key1 => 1,
